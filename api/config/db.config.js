c6e0fe37eab806b25ae3d4b63c13ff117719c903
Driztnafein <127021511+Driztnafein@users.noreply.github.com>
2023-11-01 20:47:38 +0000

    Escafolding

---
+++ api/config/db.config.js
@@ -0,0 +1,10 @@
+
+const mongoose = require('mongoose');
+
+const mongodbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/legion-perdida';
+
+mongoose.connect(mongodbUri)
+    .then(() => console.info(`Connected to database ${mongodbUri}`))
+    .catch(error => console.error(`An error ocurred trying to connect to database ${mongodbUri}`, error));
+
+    
